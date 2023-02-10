5e119c9f2331e7241e00fbf70c422a90999035db
Daniel R <110840284+Daniel-robe@users.noreply.github.com>
2023-02-10 16:18:23 -0600

    Added the questions and inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What commands should be run to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about your repo?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repo?'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What commands should be used to run tests?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL v3.0', 'BDS 3', 'None']
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
